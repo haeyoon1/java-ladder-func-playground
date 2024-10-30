65714943f27edc3106be437f0dbbadabdad2d25d
haeyoon1 <hynam1220@naver.com>
2024-10-30 22:19:05 +0900

    feat: 사다리 크기 입력 기능 추가
    - InputView 클래스 추가, Ladder 클래스 WIDTH, HEIGHT 변수 수정
    
    (cherry picked from commit 33e7691cf41742b0931580b0ef86d803d99da933)

---
+++ src/main/java/domain/Ladder.java
@@ -0,0 +1,29 @@
+package domain;
+
+import view.InputView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Ladder {
+    private final List<Line> lines;
+
+    private static final int WIDTH = InputView.getLadderWidth();
+    private static final int HEIGHT = InputView.getLadderHeight();
+
+    public Ladder() {
+        this.lines = createLadder();
+    }
+
+    private List<Line> createLadder() {
+        List<Line> ladderLines = new ArrayList<>();
+        for (int i = 0; i < HEIGHT; i++) {
+            ladderLines.add(new Line(WIDTH));
+        }
+        return ladderLines;
+    }
+
+    public List<Line> getLines() {
+        return lines;
+    }
+}
