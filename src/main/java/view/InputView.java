7e38c67ff51ef04b6ad9cd464d4e44d1bf798f48
haeyoon1 <hynam1220@naver.com>
2025-07-08 00:25:01 +0900

    feat: 서브 모듈 파일 변경

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -8,7 +8,7 @@ public class InputView {
     public static final Scanner scanner = new Scanner(System.in);
 
     public static List<String> inputPlayerNames() { // 참여자 이름 입력
-        System.out.println("참여할 사람 이름을 입력하세요. (이름은 쉼표(,)로 구분하세요)");
+        System.out.println("참여할 사람 이름을 입력하세요~ (이름은 쉼표(,)로 구분하세요)");
 
         String nameList = scanner.nextLine();
         return Arrays.asList(nameList.trim().split(","));
